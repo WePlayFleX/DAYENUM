87df49d5e0c9fb48b017583c51223d6472a89225
WePlayFleX <alex.kamai@ya.ru>
2022-01-07 01:55:21 +0300

    Добавьте файлы проекта.

---
+++ DAYENUM/DAYENUM.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+enum days_of_week {Sun, Mon, Tue, Wed, Thu, Fri, Sat};
+
+int main()
+{
+	days_of_week day1, day2;
+
+	day1 = Mon;
+	day2 = Thu;
+
+	int diff = day2 - day1;
+
+	cout << "Difference in days: " << diff << endl;
+
+	if (day1 < day2)
+	{
+		cout << "day1 comes before day2\n";
+	}
+
+	system("pause");
+	return 0;
+}
